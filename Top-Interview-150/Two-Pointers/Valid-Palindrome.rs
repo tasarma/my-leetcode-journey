0e56766431b8027300998b039d151267532d93d1
tasarma <tasarm7@protonmail.com>
2025-06-02 15:01:11 +0200

    refactored rust implementation tailored to two pointers

--- Top-Interview-150/Two-Pointers/Valid-Palindrome.rs
+++ Top-Interview-150/Two-Pointers/Valid-Palindrome.rs
@@ -4,12 +4,30 @@
 
 impl Solution {
     pub fn is_palindrome(s: String) -> bool {
-        let alphanumeric: Vec<char> = s
-            .chars()
-            .filter(|c| c.is_alphanumeric())
-            .map(|c| c.to_ascii_lowercase())
-            .collect();
+        let chars: Vec<char> = s.chars().collect();
+        let mut left = 0;
+        let mut right = chars.len() - 1;
 
-        alphanumeric == alphanumeric.iter().rev().cloned().collect::<Vec<_>>()
+        while left < right {
+            if !chars[left].is_alphanumeric() {
+                left += 1;
+            } else if !chars[right].is_alphanumeric() {
+                right -= 1;
+            } else if chars[left].to_lowercase().ne(chars[right].to_lowercase()) {
+                return false;
+            } else {
+                left += 1;
+                right -= 1;
+            }
+        }
+        true
     }
 }
+
+// let alphanumeric: Vec<char> = s
+//     .chars()
+//     .filter(|c| c.is_alphanumeric())
+//     .map(|c|  c.to_ascii_lowercase())
+//     .collect();
+
+// alphanumeric ==  alphanumeric.iter().rev().cloned().collect::<Vec<_>>()
